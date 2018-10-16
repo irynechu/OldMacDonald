9509fc12990987e92e0bb757cee4628520db3cfc
iryne <40501418+irynechu@users.noreply.github.com>
2018-10-16 14:50:28 -0700

    Update Cow.java

--- Cow.java
+++ Cow.java
@@ -1,23 +1,14 @@
-class Cow implements Animal 
-{     
-	private String myType;
-	private String mySound;
-	public Cow(String type, String sound)
-     {
-		myType = type;
-		mySound = sound;
-	}
-	public Cow()
-     {
-		myType = "unknown";
-		mySound = "unknown";
-	}
-	public String getSound() 
-     {
-		return mySound;
-	}
-	public String getType() 
-     {
-		return myType;
-	}
+class Cow implements Animal {     
+     private String myType;     
+     private String mySound;      
+     public Cow(String type, String sound)     {         
+         myType = type;         
+         mySound = sound;     
+     }     
+     public Cow()     {         
+         myType = "unknown";         
+         mySound = "unknown";     
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
 }
