31fdabc6146e5a114694b248a6b9b78618a09088
iryne <40501418+irynechu@users.noreply.github.com>
2018-10-16 14:53:47 -0700

    Update Chick.java

--- Chick.java
+++ Chick.java
@@ -1,11 +1,11 @@
-class Cow implements Animal {     
+class Chick implements Animal {     
      private String myType;     
      private String mySound;      
-     public Cow(String type, String sound)     {         
+     public Chick(String type, String sound)     {         
          myType = type;         
          mySound = sound;     
      }     
-     public Cow()     {         
+     public Chick()     {         
          myType = "unknown";         
          mySound = "unknown";     
      }      
